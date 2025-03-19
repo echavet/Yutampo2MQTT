0d27f4b117e4fcbc287685ae598919ee368ac394
Eric Chavet <echavet@gmail.com>
2025-03-19 16:29:26 +0100

    fix

--- rc-init-wrapper.sh
+++ rc-init-wrapper.sh
@@ -1,3 +1,3 @@
 # rc-init-wrapper.sh
 #!/bin/sh -e
-exec /run/s6/basedir/scripts/rc.init top /app/run.sh 2>&1 | tee /var/log/yutampo.log
+exec /run/s6/basedir/scripts/rc.init top /app/run.sh 2>&1 | tee /var/log/yutampo/current
