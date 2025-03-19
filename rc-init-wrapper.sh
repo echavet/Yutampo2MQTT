3b5b53edb07939d84d60a6a583e41dd61d00c9aa
Eric Chavet <echavet@gmail.com>
2025-03-19 16:11:55 +0100

    fix

--- rc-init-wrapper.sh
+++ rc-init-wrapper.sh
@@ -1,3 +1,3 @@
 # rc-init-wrapper.sh
 #!/bin/sh -e
-exec /run/s6/basedir/scripts/rc.init top /app/run.sh 2>&1 | s6-fdholder-store /run/s6-rc/fdholder yutampo-log
+exec /run/s6/basedir/scripts/rc.init top /app/run.sh 2>&1 | tee /var/log/yutampo.log
