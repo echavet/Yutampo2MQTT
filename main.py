5e50a884992c96129c0b50978229e932d6097ab5
Eric Chavet <echavet@gmail.com>
2025-03-19 10:50:02 +0100

    rollback

--- main.py
+++ main.py
@@ -13,11 +13,9 @@ def log_startup_message():
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     msg = f"Démarrage de l'addon Yutampo HA - {timestamp}"
     border = "*" * (len(msg) + 4)
-    # Échapper les caractères pour le shell
-    framed_message = f"\\n{border}\\n* {msg} *\\n{border}"
-    # Utiliser guillemets doubles partout avec échappement
-    command = f'/usr/bin/with-contenv bash -c "source /etc/bashio/bashio.sh && bashio::log.info \\"{framed_message}\\""'
-    subprocess.run(command, shell=True, check=True)
+    # Formatage sans codes ANSI, en s'appuyant sur le niveau de log
+    framed_message = f"{border}\n* {msg} *\n{border}"
+    LOGGER.info(framed_message)  # Utilise INFO pour imiter bashio::log.info
 
 
 if __name__ == "__main__":
